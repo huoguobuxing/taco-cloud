13f78a9077f7c1b88f4163f7f91489c65a5c7453
zhenli.liu <zhenli.liu@cision.com>
2023-04-28 14:38:23 +0800

    完成校验

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,8 @@ import tacos.Ingredient;
 import tacos.Ingredient.Type;
 import tacos.Taco;
 import tacos.TacoOrder;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -64,9 +67,12 @@ public class DesignTacoController {
 
     @PostMapping
     public String processTaco(// taco来自请求
-                              Taco taco,
+                              @Valid Taco taco, Errors errors,
                               // tacoOrder 来自model
                               @ModelAttribute TacoOrder tacoOrder) {
+        if(errors.hasErrors()){
+            return "design";
+        }
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
 
